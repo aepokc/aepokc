61e1daa78a5d8fd57939b49201074f156acf2798
Run by Sailor <admin@runbysailor.com>
2010-09-14 19:28:36 -0500

    Allow blank phone number fields.

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,7 @@ class Job < ActiveRecord::Base
 
 	belongs_to :member
 
-	validates_format_of :phone, :with => /[1-9][0-9]{9}/, :message => "- please supply the phone number in the following format: 4058182257"
-
+	validates_format_of :phone, :with => /[1-9][0-9]{9}/, 
+															:message => "- please supply the phone number in the following format: 4058182257",
+															:allow_blank => true
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,9 @@ class Profile < ActiveRecord::Base
 
 	belongs_to :member	
 	
-	validates_format_of :phone, :with => /[1-9][0-9]{9}/, :message => "- please supply the phone number in the following format: 4058182257"
+	validates_format_of :phone, :with => /[1-9][0-9]{9}/, 
+															:message => "- please supply the phone number in the following format: 4058182257",
+															:allow_blank => true
 	
 	has_attached_file :avatar, :styles => { :display => "150x150#", :thumb => "50x50#" },
 	                  :url  => "/assets/avatars/:id/:style/:basename.:extension",
