7e9100738e80cf2154416cc6acf333c9fe58baa6
Run by Sailor <admin@runbysailor.com>
2010-09-14 19:14:30 -0500

    Validate phone number format.

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,6 @@ class Job < ActiveRecord::Base
 
 	belongs_to :member
 
+	validates_format_of :phone, :with => /[1-9][0-9]{9}/, :message => "- please supply the phone number in the following format: 4058182257"
+
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,8 @@ class Profile < ActiveRecord::Base
 
 	belongs_to :member	
 	
+	validates_format_of :phone, :with => /[1-9][0-9]{9}/, :message => "- please supply the phone number in the following format: 4058182257"
+	
 	has_attached_file :avatar, :styles => { :display => "150x150#", :thumb => "50x50#" },
 	                  :url  => "/assets/avatars/:id/:style/:basename.:extension",
 	                  :path => ":rails_root/public/assets/avatars/:id/:style/:basename.:extension"
