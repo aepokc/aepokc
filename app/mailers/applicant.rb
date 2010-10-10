4a2c95acf84e5cfeb093ec70b2ba9f581b5fdc91
Run by Sailor <admin@runbysailor.com>
2010-10-10 13:43:24 -0500

    Change applicant mailer settings.

--- app/mailers/applicant.rb
+++ app/mailers/applicant.rb
@@ -2,7 +2,7 @@ class Applicant < ActionMailer::Base
 
   def mail_to_reviewer(membership_application)
 		@membership_application = membership_application
-    mail(:from => "admin@aepokc.com", :to => "acer@micahalcorn.com", :subject => "New member application")
+    mail(:from => "applicant@aepokc.com", :to => "admin@aepokc.com", :subject => "New member application")
   end
   
 end
