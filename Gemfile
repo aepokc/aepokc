8dc8644db07e229898e0e28a20fbff5e65a47d27
Run by Sailor <admin@runbysailor.com>
2010-10-17 16:06:21 -0500

    Upgrade Rails and Devise versions.

--- Gemfile
+++ Gemfile
@@ -1,13 +1,12 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.0.beta4'
-gem 'devise', '1.1.rc2'
+gem 'rails', '3.0.0'
+gem 'devise', '1.1.2'
 gem 'paperclip'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'mysql', '2.8.1'
 
 # Use unicorn as the web server
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'ad2cc17e077af862a8d4e6e2ecae981277c1755e296d554389f112cd3d78a5aa4932d92364e7720a88436b3bc9e6a74c71d5895613a6109a0681e557480ed8bd'
+Rails.application.config.secret_token = 'ad2cc17e077af862a8d4e6e2ecae981277c1755e296d554389f112cm3d78a5aa4932d92364e7720a88436b3bc9e6a74c71d5895613a6109a0681e557480ed8bd'
