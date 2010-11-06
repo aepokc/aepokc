683d6b248ce500bcb2d4db92e0391a4c39c35c27
Run by Sailor <admin@runbysailor.com>
2010-11-06 13:15:33 -0500

    Repair Event sql statements.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,23 +3,23 @@ class Event < ActiveRecord::Base
 	belongs_to :member
 
 	def current_date
-		Date.today-1
+		Date.today
 	end
 
 	def self.find_current
-		Event.find(:all, :conditions => ["date = current_date", true], :order => 'date')
+		Event.find :all, :conditions => ["date = ?", current_date], :order => 'date'
 	end
 	
 	def self.find_past
-		Event.find(:all, :conditions => ["date < current_date", true], :order => 'date desc')
+		Event.find :all, :conditions => ["date < ?", current_date], :order => 'date desc'
 	end
 	
 	def self.find_upcoming
-		Event.find(:all, :conditions => ["date > current_date", true], :order => 'date')
+		Event.find :all, :conditions => ["date > ?", current_date], :order => 'date'
 	end
 	
 	def self.find_feed
-		Event.find(:all, :conditions => ['sanctioned = true AND (date > current_date OR date = current_date)'], :order => 'date', :limit => 3)
+		Event.find :all, :conditions => ['sanctioned = true AND (date > current_date OR date = current_date)'], :order => 'date', :limit => 3
 	end
 	
 end
