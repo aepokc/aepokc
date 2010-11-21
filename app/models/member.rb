0ac868fd7c6a18005489ede5389785b96bfc2b2e
Run by Sailor <admin@runbysailor.com>
2010-11-21 15:14:13 -0600

    Require committee for new members.

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,11 +4,13 @@ class Member < ActiveRecord::Base
 	has_many :events
 	has_many :jobs
 	belongs_to :committee
+	
+	validates_presence_of :committee_name
 
   devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   		 # :token_authenticatable, :lockable and :timeoutable
 
-  attr_accessible :leader, :firstname, :lastname, :email, :password, :password_confirmation, :random_password
+  attr_accessible :leader, :firstname, :lastname, :email, :password, :password_confirmation, :random_password, :committee_name
   
 end
