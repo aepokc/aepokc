a0193367673545fa7e207ed25212a36be2b9662f
Run by Sailor <admin@runbysailor.com>
2010-12-01 16:12:05 -0600

    Expand conditions for upcoming Events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,7 +15,7 @@ class Event < ActiveRecord::Base
 	end
 	
 	def self.find_upcoming
-		Event.find :all, :conditions => ['(date > current_date)'], :order => 'date'
+		Event.find :all, :conditions => ['(date > current_date OR date = current_date)'], :order => 'date'
 	end
 	
 	def self.find_feed
