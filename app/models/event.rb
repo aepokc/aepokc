e1da79bc864a9fd470cb7d76b0367763c471c749
Run by Sailor <admin@runbysailor.com>
2010-12-16 15:29:44 -0600

    Adjust current_date for feed and remove redundant current from upcoming.

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,11 +15,11 @@ class Event < ActiveRecord::Base
 	end
 	
 	def self.find_upcoming
-		Event.find :all, :conditions => ['(date > current_date OR date = current_date)'], :order => 'date'
+		Event.find :all, :conditions => ['(date > current_date)'], :order => 'date'
 	end
 	
 	def self.find_feed
-		Event.find :all, :conditions => ['sanctioned = true AND (date > current_date OR date = current_date)'], :order => 'date', :limit => 3
+		Event.find :all, :conditions => ['sanctioned = true AND (date > current_date OR date = current_date-1)'], :order => 'date', :limit => 3
 	end
 	
 end
