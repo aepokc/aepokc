ed83e6f22e6f508c783d39e1d04b0ad607526d95
runbysailor <admin@runbysailor.com>
2011-01-18 13:15:37 -0600

    Grease up application form.

--- app/models/membership_application.rb
+++ app/models/membership_application.rb
@@ -1,7 +1,7 @@
 class MembershipApplication < ActiveRecord::Base
 
 	validates_presence_of :firstname, :lastname, :email, :address, :city, :state, :zip, :phone, 
-												:company, :title, :associations, :interests, :committee_name, :agreement
+												:company, :title, :committee_name, :agreement
 	validates_uniqueness_of :email
  
 end
