6120113b1dd7597882ebe687f30bd1c73f3390bc
Micah Alcorn <mail@micahalcorn.com>
2012-04-29 16:11:12 -0500

    Change application recipient.

--- app/mailers/applicant.rb
+++ app/mailers/applicant.rb
@@ -4,6 +4,6 @@ class Applicant < ActionMailer::Base
 		string = @membership_application.firstname + @membership_application.lastname + @membership_application.email + @membership_application.created_at.to_s
 		token = Digest::MD5.hexdigest(string)
 		@url = 'http://aepokc.com/approve?token='+token
-    mail(:from => "applicant@aepokc.com", :to => "admin@aepokc.com", :subject => "New member application")
+    mail(:from => "applicant@aepokc.com", :to => "membership@aepokc.com", :subject => "New member application")
   end
 end
