26c5c5b6d91bd900b1ae6804a93ce1bc7c9a8d1d
Micah Alcorn <mail@micahalcorn.com>
2014-10-24 14:00:51 -0500

    Ignore dump, repair deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -85,6 +85,7 @@ namespace :deploy do
       ln -s #{shared_path}/log #{latest_release}/log &&
       ln -s #{shared_path}/system #{latest_release}/public/system &&
       ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
+      ln -sf #{shared_path}/gibbon.rb #{latest_release}/config/initializers/gibbon.rb &&
       ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml
     CMD
 
